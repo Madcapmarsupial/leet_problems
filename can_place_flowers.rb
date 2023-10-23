acec3df1fe8bb4da7b187c28e728c1482b2d843e
Sam Green <samueltylergreen@gmail.com>
2023-10-23 14:06:35 -0700

    fix flowers solution

--- can_place_flowers.rb
+++ can_place_flowers.rb
@@ -1,17 +1,37 @@
 def can_place_flowers(flowerbed, n)
-  plots = 0
+   plots = 0
+   i = 0
+   while i <= flowerbed.length-1
+      if can_place_flower?(flowerbed, i)
+         plots+= 1
+         i+=2
+      else
+         i+=1
+      end
+   end
 
-  if flowerbed.length > 1
-    plots += 1 if flowerbed[0] + flowerbed[1] == 0
-    (1...flowerbed.length - 1).each do |i|
-      plots += 1  if (flowerbed[i-1] + flowerbed[i] + flowerbed[i+1]) == 0
-
-    end 
-    plots += 1 if flowerbed[-2] + flowerbed[-1] == 0
-  else
-    plots += 1 if flowerbed.first == 0
-  end
-
-  plots >= n
+   plots >= n
 end
 
+def can_place_flower?(flowerbed, i)
+   if i == 0
+    return true if flowerbed == [0]
+    return false if flowerbed.length == 1 && flowerbed.first == 1
+    #check first two elements
+    return (flowerbed[0] + flowerbed[1] == 0)
+   end
+
+   #check last two elements
+   if i == flowerbed.length-1
+      return (flowerbed[-1] + flowerbed[-2] == 0)
+   end
+  
+   # if "we still have more then two elements in the arr"
+   # check middle elements
+   #check that we dont increment i past length
+   if (flowerbed.length-1 - i) >= 1 
+      return (flowerbed[i-1] + flowerbed[i] + flowerbed[i+1]) == 0
+   end
+
+   false
+end
