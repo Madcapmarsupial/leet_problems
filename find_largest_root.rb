bd37f8ec92561b96a4fb720d13d458efdb4d6fca
Sam Green <samueltylergreen@gmail.com>
2023-11-03 15:32:01 -0700

    largest_root solution

--- find_largest_root.rb
+++ find_largest_root.rb
@@ -10,6 +10,18 @@ end
 # @param {TreeNode} root
 # @return {Integer[]}
 
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+
 def largest_values(root)
   return [] if root.nil?
     queue = [root]
@@ -18,24 +30,25 @@ def largest_values(root)
    
   until queue.empty?
     node_count = queue.size
-    #set the limit for the inner loop based of queue size
-    (0..node_count - 1).each do |i|
-      #the inner loop looks at every node in the queue and adds it's child nodes to queue (these will make up the next loop/tree level via node_count)
-      
+    #set the limit for the inner loop based off (current) queue size
+    (0..node_count - 1).each do |i|  
       node = queue.shift  
+        #remove the next node from the front of the queue
       queue << node.left unless node.left.nil?
       queue << node.right unless node.right.nil?
+        #add children to rear of the (unless they are nil)
 
-      #make sure largest node has a non-nil value before we use it
       largest_node ||= node.val
-      #check largest_node against current node
+      #if largest_node hasnt been assigned yet assign it to current node.val
       largest_node = (node.val > largest_node ? node.val : largest_node)
+      #check largest_node against current node
+       #repeat for (node_count) times
     end
 
     values << largest_node
-    #reset largest node
+    #add largest_node to values
+    #reset largest node for next loop
     largest_node = nil
   end
   values
 end
-
